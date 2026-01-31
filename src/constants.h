ebd7a7009975759b45a04fa281f872392350af3e
Ramon Meza <ramon_meza@live.com>
2026-01-31 10:16:43 -0500

    Initial commit.

---
+++ src/constants.h
@@ -0,0 +1,16 @@
+typedef struct {
+    int RAND_SEED;
+    float FIXED_UPDATE_TIME;
+    float CAMERA_SPEED;
+    float CAMERA_ROTATION_SPEED;
+    float CAMERA_ZOOM_MIN;
+    float CAMERA_ZOOM_MAX;
+    int TILE_WIDTH;
+    int TILE_HEIGHT;
+    float EMPTY_GRID_LINE_THICKNESS;
+} Configuration;
+
+const Configuration config = {
+    .RAND_SEED = 42069,
+    .FIXED_UPDATE_TIME = 1.0 / 60.0,
+};
